6c603214768629e4e55c396c2af77c69aa6a82dd
Administrator <admin@local.host>
2014-08-13 00:44:38 +0300

    Daily commit

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('lab003', ['ionic'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -17,3 +17,49 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+    .state('app', {
+      url: "/app",
+      abstract: true,
+      templateUrl: "menu.html"
+    })
+    .state('app.home', {
+      url: "/home",
+      views: {
+        'menuContent' :{
+          templateUrl: "home.html"
+        }
+      }
+    })
+    .state('app.help', {
+      url: "/help",
+      views: {
+        'menuContent' :{
+          templateUrl: "help.html",
+		  controller: 'HelpCtrl'
+        }
+      }
+    })
+  $urlRouterProvider.otherwise("/app/home");
+})
+
+.controller('HelpCtrl', function($scope) {
+  $scope.items = [
+    { title: 'iPhone 4/4s pakrovėjas', id: 1, description: 'Pakrovėjas....', available: 2, total: 10 },
+    { title: 'iPhone 5s pakrovėjas', id: 2, description: 'Pakrovėjas....', available: 5, total: 5 },
+    { title: 'MiniUSB pakrovėjas', id: 3, description: 'Pakrovėjas....', available: 0, total: 3 }
+  ];
+})
+
+.controller('menuCtrl', function($scope) {
+	$scope.menuItems = [
+		{ title: 'Home', icon: 'ion-home', href: '#app/home' },
+		{ title: 'Support', icon: 'ion-help', href: '#app/help' },
+		{ title: 'Incident', icon: 'ion-wrench', href: '#' },
+		{ title: 'Logout', icon: 'ion-log-out', href: '#' }
+	];
+})
+
